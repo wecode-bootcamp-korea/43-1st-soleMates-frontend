6470bc0f989e43f55236ffc70c1bc9af7ea4e91a
summer-o3o <ho_ho95@naver.com>
2023-03-07 17:01:27 +0900

    add : 헤더 마크업 완료 / 중분류 및 장바구니 수량 남음

---
+++ src/Components/Header/Banner/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Banner.scss';
+const Banner = () => {
+  return (
+    <div className="banner">
+      <strong className="tit_noti">Notice</strong>
+      <p className="desc_noti">로그인이 어려우실경우 FAQ를 참조해주세요</p>
+    </div>
+  );
+};
+export default Banner;
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="tit_logo">
+        <a href="/" className="link_logo">
+          <img src="../images/Header/img_logo.png" alt="" className="img_g" />
+        </a>
+      </h1>
+      <h2 className="screen_out">닥터마틴 메인 메뉴</h2>
+      <ul className="list_menu">
+        <li>
+          <a href="#none" className="link_menu">
+            신발
+          </a>
+        </li>
+        <li>
+          <a href="#none" className="link_menu">
+            신발
+          </a>
+        </li>
+        <li>
+          <a href="#none" className="link_menu">
+            신발
+          </a>
+        </li>
+        <li>
+          <a href="#none" className="link_menu">
+            신발
+          </a>
+        </li>
+      </ul>
+      <div className="util_menu">
+        <a href="#none" className="link_util">
+          <span className="ico_shop">로그인</span>
+        </a>
+        <a href="#none" className="link_util">
+          <span className="ico_shop ico_cart">장바구니</span>
+        </a>
+        <a href="#none" className="link_util">
+          <span className="ico_shop ico_wish">찜 목록</span>
+        </a>
+        <form method="post" action="#none" className="form_search">
+          <fieldset>
+            <legend className="screen_out">검색</legend>
+            <input
+              type="text"
+              className="inp_search"
+              placeholder="검색어"
+              title="검색어 입력"
+            />
+            <button type="submit" className="btn_search">
+              <span className="ico_shop ico_search">검색</span>
+            </button>
+          </fieldset>
+        </form>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
