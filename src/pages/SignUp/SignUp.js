eba84c337a43189a0f91780a2212005e788b47e7
HongTaeHoon <kdn95374888@gmail.com>
2023-03-08 16:53:40 +0900

    refactor : 회원가입 코드리뷰 일부 수정 및 데이터 연결

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Input from './Input';
 import './SignUp.scss';
 
 const SignUp = () => {
@@ -16,63 +17,77 @@ const SignUp = () => {
     setInputValues({ ...inputValues, [name]: value });
   };
 
+  const checkEmail = email.match(
+    '[a-zA-Z0-9]+[@][a-zA-Z0-9]+[.]+[a-zA-Z]+[.]*[a-zA-Z]*'
+  );
+
   const conditions = {
     name: name.length === 0 || name.length >= 2,
-    email:
-      email.length === 0 ||
-      email.match('[a-zA-Z0-9]+[@][a-zA-Z0-9]+[.]+[a-zA-Z]+[.]*[a-zA-Z]*'),
+    email: email.length === 0 || checkEmail,
     pw: pw.length === 0 || pw.length >= 4,
     pwCorrect: pw === pwCorrect,
   };
 
-  const onclick = event => {
-    if (
-      name.length >= 2 &&
-      pw.length >= 4 &&
-      pwCorrect === pw &&
-      email.match('[a-zA-Z0-9]+[@][a-zA-Z0-9]+[.]+[a-zA-Z]+[.]*[a-zA-Z]*')
-    ) {
-      alert('성공');
-    } else {
-      alert('실패');
+  const signUp = event => {
+    if (name.length >= 2 && pw.length >= 4 && pwCorrect === pw && checkEmail) {
       event.preventDefault();
+      fetch('http://10.58.52.150:8000/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify({
+          name: `${name}`,
+          email: `${email}`,
+          password: `${pw}`,
+        }),
+      })
+        .then(response => response.json())
+        .then(data => {
+          if (data.accessToken) {
+            localStorage.setItem('token', data.accessToken);
+          } else {
+            alert('중복된 이메일입니다.');
+            event.preventDefault();
+          }
+        });
     }
+
+    return null;
+  };
+
+  const condition = {
+    name: `input_${conditions.name ? 'title' : 'warn'}`,
+    email: `input_${conditions.email ? 'title' : 'warn'}`,
+    pw: `input_${conditions.pw ? 'title' : 'warn'}`,
+    pwCorrect: `input_${conditions.pwCorrect ? 'title' : 'warn'}`,
   };
 
   return (
     <form className="sign_up">
       <strong className="title">회원가입</strong>
+
+      <Input
+        condition={condition.name}
+        setInputValues={setInputValues}
+        handleInput={handleInput}
+        value={name}
+      />
+
       <div className="input_box">
-        <label className={`input_${conditions.name ? 'title' : 'warn'}`}>
-          이름
-        </label>
-        <input
-          name="name"
-          type="text"
-          className="input"
-          onChange={handleInput}
-          value={name}
-          placeholder="이름"
-        />
-      </div>
-      <div className="input_box">
-        <label className={`input_${conditions.email ? 'title' : 'warn'}`}>
-          이메일
-        </label>
+        <label className={condition.email}>이메일</label>
         <input
           name="email"
           className="input"
           type="email"
           onChange={handleInput}
-          pattern="[a-zA-Z0-9]+[@][a-zA-Z0-9]+[.]+[a-zA-Z]+[.]*[a-zA-Z]*"
+          pattern={checkEmail}
           value={email}
           placeholder="이메일"
         />
       </div>
       <div className="input_box">
-        <label className={`input_${conditions.pw ? 'title' : 'warn'}`}>
-          비밀번호
-        </label>
+        <label className={condition.pw}>비밀번호</label>
         <input
           name="pw"
           className="input"
@@ -84,9 +99,7 @@ const SignUp = () => {
         />
       </div>
       <div className="input_box">
-        <label className={`input_${conditions.pwCorrect ? 'title' : 'warn'}`}>
-          비밀번호 확인
-        </label>
+        <label className={condition.pwCorrect}>비밀번호 확인</label>
         <input
           name="pwCorrect"
           className="input"
@@ -98,7 +111,7 @@ const SignUp = () => {
         />
       </div>
 
-      <button onClick={onclick} type="submit" className="create_id">
+      <button onClick={signUp} type="submit" className="create_id">
         회원가입
       </button>
     </form>
