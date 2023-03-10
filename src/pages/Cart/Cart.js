ab4b2ddc6c6b3db4650a2c543b3060c034435588
summer-o3o <ho_ho95@naver.com>
2023-03-10 12:21:17 +0900

    update : 장바구니 임시 커밋

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,19 +1,51 @@
 import React from 'react';
+import EmptyCart from './EmptyCart/EmptyCart';
+import ItemChoice from '../../Components/ItemChoice/ItemChoice';
 import './Cart.scss';
 
 const Cart = () => {
   return (
     <div className="cart">
       <h2 className="tit_cart">장바구니</h2>
-      <div className="empty_cart">
-        <strong class="tit_empty">장바구니에 담긴 상품이 없습니다.</strong>
-        <p className="desc_cart">
-          척 70 무브 첫테일러, 원스타, 런스타 등<br />
-          지금 컴버스의 다양한 상품을 찾아보세요.
-        </p>
-        <a href="#none" className="btn_cart">
-          쇼핑계속하기
-        </a>
+      {/* <EmptyCart /> 장바구니가 비어있을때 */}
+      <div className="group_cart">
+        <div className="wrap_listcart">
+          <div className="box_cart">
+            신규 회원가입시, 즉시 사용가능한 1만원 프로모션 코드를
+            발급해드립니다.
+          </div>
+          <strong className="screen_out">장바구니에 담긴 상품 목록</strong>
+          <ItemChoice checkbox={'전체선택'} checkId={'checkAll'} />
+          <ul className="list_cart">
+            <li>
+              <ItemChoice checkId={'check01'} />
+              <span className="item_thumb">
+                <img
+                  src="../images/Product/@thumb_266x334.png"
+                  className="img_g"
+                  alt=""
+                />
+              </span>
+              <span className="item_info">
+                <strong className="tit_name">
+                  <span className="screen_out">상품명 : </span>척 테일러 올스타
+                  크래프티드 패치워크 에그렛척 테일러 올스타 크래프티드 패치워크
+                  에그렛척 테일러 올스타 크래프티드 패치워크 에그렛척 테일러
+                  올스타 크래프티드 패치워크 에그렛
+                </strong>
+                <span className="txt_opt">
+                  [옵션 : 에그렛 /<span className="screen_out">사이즈 : </span>{' '}
+                  220]
+                </span>
+                <span className="num_count">
+                  <span className="screen_out">담긴 </span>수량 2
+                  <span className="screen_out">개 </span>
+                </span>
+              </span>
+            </li>
+          </ul>
+        </div>
+        <div className="wrap_order"></div>
       </div>
     </div>
   );
