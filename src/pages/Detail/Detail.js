2e9092370c3ccd6abc3b55a7b8214d7dd8812b43
HongTaeHoon <kdn95374888@gmail.com>
2023-03-10 21:28:13 +0900

    update : 구매 후기 마크업 추가 및 구매수량 개수 제한 추가

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -2,7 +2,12 @@ import React, { useState } from 'react';
 import './Detail.scss';
 
 const Detail = () => {
-  const [data, setData] = useState({ name: '', count: '', size: '' });
+  const [data, setData] = useState({
+    name: '',
+    quantity: '',
+    size: '',
+    color: '',
+  });
   const [count, setCount] = useState(1);
   const price = 260_000;
   const totalPrice = price * count;
@@ -10,109 +15,136 @@ const Detail = () => {
     count > 1 ? setCount(count - 1) : setCount(1);
   };
   const onclick = () => {
-    setCount(count + 1);
+    count < 99 ? setCount(count + 1) : setCount(99);
   };
 
-  // const [changeButtonColor, setChangeButtonColor] = useState('');
-
   const click = event => {
-    // changeButtonColor === ''
-    //   ? setChangeButtonColor('check')
-    //   : setChangeButtonColor('');
-    // console.log(event.target.className);
     setData(prev => ({ ...prev, size: event.target.title }));
     console.log(event);
   };
-  console.log(SHOSE_SIZE.map(list => list.size));
+
+  const handleColor = event => {
+    setData(prev => ({ ...prev, color: event.target.title }));
+    console.log(event);
+  };
   console.log(data);
 
   return (
-    <div className="detail">
-      <div className="product_img_list">
-        <p id="product_img_1" />
-        {IMG_LIST.map(list => {
-          return (
-            <img
-              key={list.id}
-              className="product_img"
-              src={list.src}
-              alt="soleMates"
-            />
-          );
-        })}
-      </div>
-      <div className="product_detail">
-        <div className="product_detail_option">
-          <div className="icon">
-            <span className="ico_shop ico_sharing">공유하기</span>
-            <span className="ico_shop ico_like">좋아요</span>
-            <strong className="how_many_like">547</strong>
-          </div>
-          <strong className="product_name">1460스무스</strong>
+    <>
+      <div className="detail">
+        <div className="product_img_list">
+          <p id="product_img_1" />
+          {IMG_LIST.map(list => {
+            return (
+              <img
+                key={list.id}
+                className="product_img"
+                src={list.src}
+                alt="soleMates"
+              />
+            );
+          })}
+        </div>
+        <div className="product_detail">
+          <div className="product_detail_option">
+            <div className="icon">
+              <span className="ico_shop ico_sharing">공유하기</span>
+              <span className="ico_shop ico_like">좋아요</span>
+              <strong className="how_many_like">547</strong>
+            </div>
+            <strong className="product_name">1460스무스</strong>
 
-          <div className="product_size">
-            {SHOSE_SIZE.map(list => {
-              const buttonCheckValue =
-                data.size === String(list.size) ? 'check' : '';
-              return (
+            <div className="product_size">
+              {SHOSE_SIZE.map(list => {
+                const buttonCheckValue =
+                  data.size === String(list.size) ? 'check' : '';
+                return (
+                  <button
+                    onClick={click}
+                    key={list.id}
+                    title={list.size}
+                    className={`product_size_button ${buttonCheckValue}`}
+                  >
+                    {list.size}
+                  </button>
+                );
+              })}
+            </div>
+            <button>
+              {IMG_LIST.map(list => {
+                const selectProductColor =
+                  data.color === String(list.color) ? 'color' : '';
+                return (
+                  <img
+                    key={list.id}
+                    onClick={handleColor}
+                    title={list.color}
+                    className={`select_product ${selectProductColor} `}
+                    src={list.src}
+                    alt="soleMates"
+                  />
+                );
+              })}
+            </button>
+            <div className="product_count">
+              <div className="product_count">
+                <strong className="product_amount">수량</strong>
+                <strong className="product_amount">{count}</strong>
                 <button
-                  onClick={click}
-                  key={list.id}
-                  title={list.size}
-                  className={`product_size_button ${buttonCheckValue}`}
+                  className="product_minus"
+                  onClick={() => {
+                    handleCount();
+                  }}
                 >
-                  {list.size}
+                  −
                 </button>
-              );
-            })}
+                <button className="product_plus" onClick={onclick}>
+                  +
+                </button>
+              </div>
+              <span className="product_price"> ₩ </span>
+              <span className="product_price">
+                {totalPrice
+                  .toString()
+                  .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')}
+              </span>
+            </div>
+            <div className="button_list">
+              <button className="cart">장바구니</button>
+              <button className="buy">구매하기</button>
+            </div>
           </div>
-          <button>
-            {IMG_LIST.map(list => {
-              return (
+        </div>
+
+        <div className="detail_navigate">
+          <a className="link_navigate" href="#product_img_1">
+            Buy Now
+            <strong className="link_navigate_ko">구매하기</strong>
+          </a>
+          <a className="link_navigate" href="#review">
+            Review
+            <strong className="link_navigate_ko">상품 후기</strong>
+          </a>
+        </div>
+      </div>
+      <div id="review">
+        <div className="review">상품 후기</div>
+        <div className="photo_review">포토리뷰</div>
+        <ul className="photo_flex">
+          {IMG_LIST.map(list => {
+            return (
+              <button className="modal_photo_review" key={list.id}>
                 <img
-                  key={list.id}
-                  className={`select_product_color ${list.color}`}
+                  className="photo_review_list"
                   src={list.src}
                   alt="soleMates"
                 />
-              );
-            })}
-          </button>
-          <div className="product_count">
-            <div className="product_count">
-              <strong className="product_amount">수량</strong>
-              <strong className="product_amount">{count}</strong>
-              <button
-                className="product_minus"
-                onClick={() => {
-                  handleCount();
-                }}
-              >
-                −
               </button>
-              <button className="product_plus" onClick={onclick}>
-                +
-              </button>
-            </div>
-            <span className="product_price"> ₩ </span>
-            <span className="product_price">
-              {totalPrice
-                .toString()
-                .replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',')}
-            </span>
-          </div>
-          <div className="button_list">
-            <button className="cart">장바구니</button>
-            <button className="buy">구매하기</button>
-          </div>
-        </div>
+            );
+          })}
+        </ul>
       </div>
-      <div>
-        <a className="buy_now" href="#product_img_1">
-          Buy Now
-        </a>
-      </div>
-    </div>
+    </>
   );
 };
 export default Detail;
