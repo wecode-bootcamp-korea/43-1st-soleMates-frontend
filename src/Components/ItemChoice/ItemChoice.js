f6b0c3a87d1a988517c8c7abb256ae830adca29d
summer-o3o <ho_ho95@naver.com>
2023-03-10 12:04:45 +0900

    add : 체크박스 컴포넌트 추가

---
+++ src/Components/ItemChoice/ItemChoice.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './ItemChoice.scss';
+
+const ItemChoice = props => {
+  const { checkbox, checkId } = props;
+  return (
+    <span className="item_choice">
+      {/*
+        체크박스 마다 id값과  htmlFor을 동일하게 변경해주세요.
+        id과  htmlFor는 체크박스마다 달라야합니다.
+        
+        이해가 안가신다면 김수미에게 슬랙 주세요.
+      */}
+      <input type="checkbox" id={checkId} className="inp_check" />
+      <label htmlFor={checkId} className="lab_check">
+        <span className="ico_shop ico_check"></span>
+      </label>
+      {checkbox && <span className="txt_choice">전체선택</span>}
+    </span>
+  );
+};
+
+export default ItemChoice;
