5b65419d217a96a621c6b0e8e29f44d2f0897a29
summer-o3o <ho_ho95@naver.com>
2023-03-10 12:24:03 +0900

    update : 실수로 들어간 cart 마크업삭제

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,22 +1,6 @@
 import React from 'react';
 import './Cart.scss';
 
-const Cart = () => {
-  return (
-    <div className="cart">
-      <h2 className="tit_cart">장바구니</h2>
-      <div className="empty_cart">
-        <strong class="tit_empty">장바구니에 담긴 상품이 없습니다.</strong>
-        <p className="desc_cart">
-          척 70 무브 첫테일러, 원스타, 런스타 등<br />
-          지금 컴버스의 다양한 상품을 찾아보세요.
-        </p>
-        <a href="#none" className="btn_cart">
-          쇼핑계속하기
-        </a>
-      </div>
-    </div>
-  );
-};
+const Cart = () => {};
 
 export default Cart;
